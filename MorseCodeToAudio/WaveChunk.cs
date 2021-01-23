25338db1be2663656544e203e7b2a05c7ac72497
AVK <vamshikrishna.naidu@live.com>
2021-01-23 23:37:00 +0530

    Add project files.

---
+++ MorseCodeToAudio/WaveChunk.cs
@@ -0,0 +1,10 @@
+﻿namespace MorseCodeToAudio
+{
+    abstract class WaveChunk
+    {
+        public char[] ChunkId { get; set; }
+        public uint ChunkSize { get; set; }
+
+        public abstract byte[] ToBytes();
+    }
+}
